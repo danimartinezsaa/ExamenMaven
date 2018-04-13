fe1db1bc8ecef41b1afadaa99a126f7e396ca9c3
Daniel Martinez <dani@danielcastelao.org>
2018-04-13 09:21:09 +0200

    Implementados métodos para obtener título de la página y temperatura.

---
+++ src/main/java/com/dani/examenmaven/Metodos.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dani.examenmaven;
+
+import java.io.IOException;
+import static java.rmi.server.LogStream.log;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * Clase con métodos para sacar el título de una web y para sacar código HTML.
+ * @author dani
+ */
+public class Metodos{
+    /**
+     * Método para sacar el título de "https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP
+     */
+    public static void sacarTitulo(){
+        try{
+            Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
+            String title = doc.title();
+            System.out.println(title);
+        }catch(IOException ex){
+            System.out.println("Error:"+ex);
+        }
+    }
+    /**
+     * Método para sacar el tiempo actual en Vigo.
+     */
+    public static void pasarTiempo(){
+        try{
+            Document doc=Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
+            String tiempo=doc.select(".today_nowcard-phrase").text();
+            String temperatura=doc.select(".today_nowcard-temp").text();
+            System.out.println(tiempo);
+            System.out.println(temperatura);
+        }catch(IOException ex){
+            System.out.println("Error:"+ex);
+        }
+
+    }
+}
